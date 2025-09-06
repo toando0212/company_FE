5d02aba7f53f8e6d0cfc60418d0b8cd4f84d003f
toan0212 <toando0212@gmail.com>
2025-09-06 16:27:38 +0700

    add transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,20 +8,43 @@ import Services from './pages/Services';
 import Projects from './pages/Projects';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import React, { useRef } from 'react';
 import './css/about-header.css';
+import './css/transitions.css';
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  const nodeRef = useRef(null);
+  return (
+    <SwitchTransition mode="out-in">
+      <CSSTransition
+        key={location.pathname}
+        classNames="fade"
+        timeout={300}
+        nodeRef={nodeRef}
+        unmountOnExit
+      >
+        <div ref={nodeRef}>
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </CSSTransition>
+    </SwitchTransition>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <AnimatedRoutes />
       </Layout>
     </Router>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,15 +3,17 @@ import type { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
+
 interface LayoutProps {
   children: ReactNode;
 }
+
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="app-bg">
       <Header />
-      <main>
+      <main style={{ minHeight: '60vh' }}>
         {children}
       </main>
       <Footer />
